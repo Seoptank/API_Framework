58d34feb5fa8a77e13b63623d2435382a17ee9f7
Seoptank <cnpdi1118@naver.com>
2023-05-19 13:20:56 +0900

    Update map.cpp

--- iterator/map/map.cpp
+++ iterator/map/map.cpp
@@ -5,23 +5,69 @@
 using namespace std;
 
 
-typedef struct Score
+typedef struct tagScore
 {
-	string name;
+	string Name;
 
 	int Kor;
 	int Eng;
 	int Math;
 
-	Score():Kor(0),Eng(0),Math(0){}
+	//** 생성자↓: 생성자는 생성하고자 할때는 무조건 생성돼야함.
+	tagScore():Name(""), Kor(0), Eng(0), Math(0) {}
 
-	Score(int _kor, int _eng, int _math) :
-		Kor(_kor), Eng(_eng), Math(_math) {}
+	//** 복사생성자↓
+
+	tagScore(string _name) :
+		Name(_name), Kor(0), Eng(0), Math(0) {}
+
+	tagScore(int _kor, int _eng, int _math) :
+		Name(""), Kor(_kor), Eng(_eng), Math(_math) {}
 	
-};
+	tagScore(string _name, int _kor, int _eng, int _math) :
+		Name(_name),Kor(_kor), Eng(_eng), Math(_math) {}
+
+	//복사 생성자를 사용하려면 기본 생성자가 꼭 정의돼야한다.
+}Score;
+
+map<string, list<Score>> studentList;
+
+list<Score> scoreList; 
+
+// bool로 쓴 이유를 생각하자.
+bool AddStudent(string _key, string _name,int _kor,int _eng,int _math )
+{
+	map<string, list<Score>>::iterator iter = studentList.find(_key);
+
+	if (_name == "")
+		return false;
+
+	Score score = Score(_name,10, 20, 30);
+
+	if (iter == studentList.end())
+	{
+		list<Score> tempList;
+		tempList.push_back(score);
+		studentList.insert(make_pair(_key, tempList));
+	}
+	else
+		iter->second.push_back(score);
+
+	return true;
+}
 
 int main(void)
 {
+	//오른쪽의 생성자를 왼쪽의 s변수에 호출하여 생성
+	// 생성자 호출이 없다면 생성도 안됨
+	Score s = Score(10,20,30);
+	// ↓
+	Score s1;//생성자가 생략된 형태
+
+	Score s2(s);
+	// 기본 생성자와 기본 복사 생성자는 위처럼 따로 정의하지 않더라도
+	// 기본적으로 정의되어있기에 사용할 수 있다.
+
 	/*
 	map<string, Score> mapList;
 
@@ -48,7 +94,7 @@ int main(void)
 
 	multiList.insert(make_pair(key, score));
 	multiList.insert(make_pair(key, score));
-
+	
 	for (multimap<string, Score>::iterator iter = multiList.begin(); iter != multiList.end(); ++iter)
 	{
 		cout << "[" << iter->first << "]" << endl;
@@ -65,12 +111,41 @@ int main(void)
 	//** multimap : 
 	*/
 
-	map<string, list<Score>> studentList;
 
 	string key = "홍";
-	Score score = Score(10, 20, 30);
-	score.name = "길동";
+	string name = "길동";
+
+	
+
+	if (!AddStudent(key, name , 10, 20, 30))
+		cout << "Log" << endl;
+/*
+	else
+		cout << studentList[key].front().Name << endl;
+*/
+
+
+	for (map<string,list<Score>>::iterator iter = studentList.begin();
+		iter != studentList.end();++iter)
+	{
+		cout << "[" << iter->first + name << "]" << endl;
+
+		scoreList = iter->second; // scoreList에 iter->second를 줌
+
+		for (list<Score>::iterator itera = scoreList.begin();
+			itera != scoreList.end(); ++itera)
+		{
+			cout << "국어점수" << ":" << itera->Kor << endl;
+			cout << "영어점수" << ":" << itera->Eng << endl;
+			cout << "수학점수" << ":" << itera->Math << endl;
+		}
+		
+	}
+
 
 
 	return 0;
 }
+
+
+
